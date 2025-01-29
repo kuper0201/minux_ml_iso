2633c5a6a0276e055b8eba88c5484de31e33c748
kuper0201 <kuper171@gmail.com>
2025-01-29 16:03:59 +0900

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="archlinux"
+iso_name="minux"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue DVD"
@@ -24,5 +24,4 @@ file_permissions=(
   ["/usr/local/bin/setup-minux"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
 )
