cd212549b36a4160396ff7babaf78261fc57df59
kuper0201 <kuper171@gmail.com>
2025-04-20 01:49:15 +0900

    Update profiledef.sh

--- profiledef.sh
+++ profiledef.sh
@@ -22,7 +22,6 @@ file_permissions=(
   ["/root/.automated_script.sh"]="0:0:755"
   ["/root/.gnupg"]="0:0:700"
   ["/usr/local/bin/setup-minux"]="0:0:755"
-  ["/usr/local/bin/interactive-installation"]="0:0:755"
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
 )
